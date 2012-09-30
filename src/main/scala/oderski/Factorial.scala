8c8ad80e2800605ae8f1fef8964608abc5938bca
Henry Story <henry.story@bblfish.net>
2012-09-30 18:17:42 +0200

    A more dynamic version of the RequestHeader work

---
+++ src/main/scala/oderski/Factorial.scala
@@ -0,0 +1,12 @@
+package oderski
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hjs
+ * Date: 26/09/2012
+ * Time: 20:46
+ * To change this template use File | Settings | File Templates.
+ */
+class Factorial {
+
+}
---
+++ src/main/scala/play/DynamicServer.scala
@@ -0,0 +1,12 @@
+package play
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hjs
+ * Date: 28/09/2012
+ * Time: 16:20
+ * To change this template use File | Settings | File Templates.
+ */
+class DynamicServer {
+
+}
